5fb778e9ffc6d8bd06c26639d4286bdb19452ca7
Paulo Victor Moraes da Silva <55352214+paulovictorBraw@users.noreply.github.com>
2022-01-15 23:12:56 -0300

    feat(services) listagem das noticias e criação de uma noticia

--- src/app/pages/news/news.page.ts
+++ src/app/pages/news/news.page.ts
@@ -2,6 +2,8 @@ import { AddNewComponent } from './component/add-new/add-new.component';
 import { NewDetailComponent } from './component/new-detail/new-detail.component';
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { NewsService } from './../../services/news/news.service';
+import { NewsModel } from './../../models/news.model';
 
 @Component({
   selector: 'app-news',
@@ -9,12 +11,20 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./news.page.scss'],
 })
 export class NewsPage implements OnInit {
-
+  news: NewsModel[];
   constructor(
+    private newsService: NewsService,
     private modalCtrl: ModalController
   ) { }
 
   ngOnInit() {
+    this.newsService.getNews().subscribe((res) => {
+      this.news = res.map((t) => ({
+          id: t.payload.doc.id,
+          ...t.payload.doc.data() as NewsModel
+        }));
+        console.log(this.news);
+    });
   }
 
   public async openNewDetail(): Promise<void> {
