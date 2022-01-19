ecdc42173f1885aa6aeac61d3f51982a5277371d
ewerton_ricardo <ewerton.ricardo98@gmail.com>
2022-01-18 21:08:16 -0300

    - Implemented the gallery layout

--- src/app/pages/gallery/gallery.module.ts
+++ src/app/pages/gallery/gallery.module.ts
@@ -1,3 +1,4 @@
+import { SharedComponentsModule } from './../../components/shared-components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -13,7 +14,8 @@ import { GalleryPage } from './gallery.page';
     CommonModule,
     FormsModule,
     IonicModule,
-    GalleryPageRoutingModule
+    GalleryPageRoutingModule,
+    SharedComponentsModule
   ],
   declarations: [GalleryPage]
 })
