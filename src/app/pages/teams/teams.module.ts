6984db687c4798b8ecb66be25a4c02241e0b805a
ewerton_ricardo <ewerton.ricardo98@gmail.com>
2022-01-30 16:40:34 -0300

    - Created the add-team modal component  - Removed the unused class

--- src/app/pages/teams/teams.module.ts
+++ src/app/pages/teams/teams.module.ts
@@ -1,3 +1,4 @@
+import { AddTeamComponent } from './components/add-team/add-team.component';
 import { SharedComponentsModule } from './../../components/shared-components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -17,6 +18,9 @@ import { TeamsPage } from './teams.page';
     TeamsPageRoutingModule,
     SharedComponentsModule
   ],
-  declarations: [TeamsPage]
+  declarations: [
+    TeamsPage,
+    AddTeamComponent
+  ]
 })
 export class TeamsPageModule {}
--- src/app/pages/teams/teams.page.ts
+++ src/app/pages/teams/teams.page.ts
@@ -1,4 +1,6 @@
+import { AddTeamComponent } from './components/add-team/add-team.component';
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-teams',
@@ -31,9 +33,18 @@ export class TeamsPage implements OnInit {
       ]
     }
   ];
-  constructor() { }
+  constructor(
+    private modalCtrl: ModalController
+  ) { }
 
   ngOnInit() {
+  }
+
+  public async openAddTeamModal(): Promise<void> {
+    const modal = await this.modalCtrl.create({
+      component: AddTeamComponent,
+    });
+    await modal.present();
   }
 
 }
