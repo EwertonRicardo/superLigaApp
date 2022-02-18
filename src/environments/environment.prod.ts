16f91de37e161527412be261dbe4d4bd84f4cc4c
ewerton_ricardo <ewerton.ricardo98@gmail.com>
2022-02-18 20:43:43 -0300

    fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyC65Rq48I3FSD4pugIRRjpqb2DYPicKGeA',
-    authDomain: 'superligaapp-66384.firebaseapp.com',
-    projectId: 'superligaapp-66384',
-    storageBucket: 'superligaapp-66384.appspot.com',
-    messagingSenderId: '108928368596',
-    appId: '1:108928368596:web:017555222fb5c02a67a041',
-    measurementId: 'G-568J2YNM20'
+    apiKey: 'AIzaSyCkTVIHcSr2u19LCeNfl7MHBukmv-8gPQI',
+    authDomain: 'superligaapp-2fba7.firebaseapp.com',
+    projectId: 'superligaapp-2fba7',
+    storageBucket: 'superligaapp-2fba7.appspot.com',
+    messagingSenderId: '582568991901',
+    appId: '1:582568991901:web:c70e9a29e8561cc00e8874',
+    measurementId: 'G-8E7E69K1DX'
   }
 };
